2ef2dfffedb059f5b6e413c462c797a84a71d146
Chigozie Ugwuede <sparkyglobe@gmail.com>
2020-11-20 04:57:09 +0100

    Register user to the database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,86 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const User = require("../../model/User");
 
 // @route GET api/users
-// @desc Test  route
+// @desc Register user
 // @access public
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Password must be a min of six characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { name, email, password } = req.body;
+
+    // check iif users exist
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({
+          errors: [{ msg: "User already exists" }],
+        });
+      }
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Servver error");
+    }
+
+    // get user's gravata
+    const avatar = gravatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "mm",
+    });
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password,
+    });
+
+    // encrypt the password
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    // retuurn jsonwentoken
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get("jwtSecret"),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+app.use(express.json({ extended: false }));
+
 // Connecting Database
 connectDB();
 
