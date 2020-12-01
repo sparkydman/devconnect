5aaca616a7a6d0b4d6e5e257ac339c21a4cc9dcd
Chigozie Ugwuede <sparkyglobe@gmail.com>
2020-12-01 18:08:13 +0100

    Preparing for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -32,6 +33,15 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
+
+// server static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.POST || 5000;
 
